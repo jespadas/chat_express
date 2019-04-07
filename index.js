482d02143ba4df88528d1384a90612ecbdffbe7f
Julio Espadas <julio.espadas@gmail.com>
2019-04-07 20:26:39 +0200

    chat working

--- index.js
+++ index.js
@@ -1,24 +1,24 @@
-const express = 'require'('express')
-const app = express()
+const express = require('express');
+const index = express();
 
 //set the template engine ejs
-app.set('View engine','ejs')
+index.set('View engine','ejs');
 
 //middlewares
-app.use(express.static('public'))
+index.use(express.static('public'));
 
 //routes
-app.get('/',(req,res))=>{
+index.get('/',(req, res)=>{
     res.send('index')
-})
+});
 
 //listen on port 3000
-server = app.listen(3000)
+server = index.listen(3000);
 
 //socket.io instantiation
-const io = require('socket.io')(server)
+const io = require('socket.io')(server);
 
 //listen on every connection
 io.on('connection',(socket)=>{
     console.log('New user connected')
-})
+});
